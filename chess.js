5ec7d7e7ba26d1fe056e3767f3ea1cdff1503c3f
Jonathan Frederick <jrfreder@gmail.com>
2016-09-05 22:48:33 -0400

    passing 8 row test

--- chess.js
+++ chess.js
@@ -1,4 +1,12 @@
 function Board() {
+    this.squares = new Array();
+    var i, j;
+    for (i = 0; i < 8; i++){
+        this.squares.push([]);
+        for (j = 0; j < 8; j++){
+            this.squares[i][j] = false;
+        };
+    };
 };
 
 module.exports = {Board: Board};
