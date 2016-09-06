f079c676e5e619ee494e1bc1843de6517e821ef1
Jonathan Frederick <jrfreder@gmail.com>
2016-09-05 21:47:54 -0400

    testing for 8 rows

--- test/test-board.js
+++ test/test-board.js
@@ -3,3 +3,11 @@ casper.test.begin("Hello, Test!", 1, function(test) {
     test.assert(true);
     test.done();
 });
+
+var chess = require("../chess.js");
+
+casper.test.begin("Board has 8 rows and 8 columns", 1, function suite(test){
+    var board = new chess.Board();
+    test.assertEquals(board.squares.length, 8);
+
+});
